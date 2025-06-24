bb4e5c4534fd700f080d764d731eaf3720f72b9e
kraneelManandhar <mydarling3547@gmail.com>
2025-06-24 14:56:51 +0545

    completed with html and js (learned blob in js)

--- index.js
+++ index.js
@@ -1,4 +1,46 @@
-let json_file = document.getElementsById("");
-let json_edit = document.getElementById("jsontxt");
+let fileInput = document.getElementById("file");
+let jsonEditor = document.getElementById("jsontxt");
+let downloadLink = document.getElementById("save");
+// console.log(fileInput.files);
 
-console.log(json_file);
+fileInput.addEventListener("change", function () {
+    const file = fileInput.files[0];//gets file that we dropped in html
+    //as array store hanxa so indexing [0]
+    // console.log("This is the file \n" ,file);
+
+    const reader = new FileReader();//build in 
+
+    reader.addEventListener("load", (kraneel) => {
+        try {
+            // console.log(kraneel);
+            const json = JSON.parse(kraneel.target.result);//turns json to javascript obj
+
+            jsonEditor.value = JSON.stringify(json);
+        //turns js obj to json string 
+        } catch (err) {
+            alert("Invalid JSON file.");
+        }
+    });
+
+    if (file !== undefined && file !== null) {
+    reader.readAsText(file);
+}
+});
+
+function saveJSON() {
+    try {
+        const editedJson = JSON.parse(jsonEditor.value);
+        console.log(editedJson);
+        console.log(JSON.stringify(editedJson));
+
+        
+        const blob = new Blob([JSON.stringify(editedJson)], { type: "application/json" });//used blob as i haven't learned node js but will start as soon as this project completes
+        console.log(downloadLink);
+        downloadLink.href = URL.createObjectURL(blob);
+        downloadLink.download = "naya.json";
+        downloadLink.textContent = "Click here to download your edited JSON file";
+
+    } catch (err) {
+        alert("Invalid JSON format.");
+    }
+}
